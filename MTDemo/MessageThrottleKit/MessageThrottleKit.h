ec6949a1dc33e0fdc9047c463fdbd0a46e9d6116
yulingtianxia <yulingtianxia@gmail.com>
2017-11-05 03:18:02 +0800

    update

--- MTDemo/MessageThrottleKit/MessageThrottleKit.h
+++ MTDemo/MessageThrottleKit/MessageThrottleKit.h
@@ -16,5 +16,3 @@ FOUNDATION_EXPORT const unsigned char MessageThrottleKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MessageThrottleKit/PublicHeader.h>
 
-#import <MessageThrottleKit/MessageThrottle.h>
-
