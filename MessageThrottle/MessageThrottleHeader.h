90be3095b96f78792d454775579f9803a602f4bc
kevinli <kevinlzh1108>
2025-07-15 14:29:23 +0800

    podspec新增subspec，统一配置放最外层，kMTPersistentRulesKey不对外暴露，新增MessageThrottleHeader暴露头文件

---
+++ MessageThrottle/MessageThrottleHeader.h
@@ -0,0 +1,10 @@
+//
+//  MessageThrottleHeader.h
+//  MTDemo
+//
+//  Created by renektonli on 2025/7/14.
+//  Copyright © 2025 杨萧玉. All rights reserved.
+//
+
+#import "MessageThrottle.h"
+#import "MTEngine+MTArchive.h"
